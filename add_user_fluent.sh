1290d7a9f1183951442025f65748855f600ada35
Tony.Shao <xiocode@gmail.com>
2016-07-05 16:31:07 +0800

    update

---
+++ add_user_fluent.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+PUID=${PUID:-911}
+PGID=${PGID:-911}
+
+if [ ! "$(id -u fluent)" -eq "$PUID" ]; then usermod -o -u "$PUID" fluent ; fi
+if [ ! "$(id -g fluent)" -eq "$PGID" ]; then groupmod -o -g "$PGID" fluent ; fi
