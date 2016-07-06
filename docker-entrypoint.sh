0bb870da48d24b10dacb0b2b8691c8c968aaf949
Tony.Shao <xiocode@gmail.com>
2016-07-06 16:32:03 +0800

    update

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -9,10 +9,10 @@ PUID=${PUID:-1001}
 PGID=${PGID:-1001}
 
 if [ ! "$(id -u fluent)" -eq "$PUID" ]; then
-sudo usermod -o -u $PUID $USER
+sudo su -c "usermod -o -u $PUID $USER"
 fi
 if [ ! "$(id -g fluent)" -eq "$PGID" ]; then
-sudo groupmod -o -g $PGID $GROUP
+sudo su -c "groupmod -o -g $PGID $GROUP"
 fi
 
 # if [ "${1:0:1}" = '-' ]; then
